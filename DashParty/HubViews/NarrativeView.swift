3ad51cbd266c35da825ffe838b7c6351a6ee96c3
LuanaPueno <luanabuenorj@gmail.com>
2025-03-24 16:29:26 -0300

    organized assets and initiated narrative view hub

--- DashParty/HubViews/NarrativeView.swift
+++ DashParty/HubViews/NarrativeView.swift
@@ -11,10 +11,28 @@ import SwiftUI
 struct NarrativeView: View {
     var body: some View {
         ZStack{
-            Image("narrative1")
+            Image("narrativeBackground")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Image("narrativeSkipButton")
+                
+                ZStack{
+                    Image("narrativeTextBackground")
+                        
+                    
+                    VStack{
+                        Text("A cada geração, a floresta Aru escolhe seu líder...")
+                        HStack{
+                            Spacer()
+                            Image("nextNarrativeButton")
+                        }
+                    }
+                }
+            }
+            
         }
     }
 }
