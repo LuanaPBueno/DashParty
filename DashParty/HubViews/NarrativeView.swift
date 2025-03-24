ce53ef0d2f6bf5943d17e3ab1cb23956e6161a0b
madumariano <madumarianofernandez@gmail.com>
2025-03-24 17:31:26 -0300

    font in the narrative text

--- DashParty/HubViews/NarrativeView.swift
+++ DashParty/HubViews/NarrativeView.swift
@@ -9,34 +9,53 @@ import Foundation
 import SwiftUI
 
 struct NarrativeView: View {
+    
     var body: some View {
+        
         ZStack{
+            
             Image("narrativeBackground")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
             
+            
             VStack{
                 Image("narrativeSkipButton")
-                    .background(Color(.white))
                     .frame(width: 100, height: 100)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                    .padding(.top, 20)
+                    .padding(.trailing, 40)
                 
-                ZStack{
-                    Image("narrativeTextBackground")
-                    
-                    VStack{
-                        Text("A cada geração, a floresta Aru escolhe seu líder...")
-                            .foregroundColor(.black)
-                        HStack{
-                            Spacer()
-                            Image("nextNarrativeButton")
-                                .background(Color(.white))
-                                .frame(width: 100, height: 100)
+                VStack{
+                                    
+                    ZStack{
+                        Image("narrativeTextBackground")
+                            .padding(.bottom, 350)
+                            .frame(maxWidth: 1280)
+                            
+                        VStack {
+                            Text("A cada geração, a floresta Aru escolhe seu líder...")
+                                .foregroundColor(.black)
+                                .foregroundColor(.black)
+                                .font(.custom("Prompt-Regular", size: 45))
+                                .font(.system(size: 22, weight: .medium))
+                                .multilineTextAlignment(.center)
+                                .padding(.bottom, 30)
+                            
+                            HStack{
+                                Spacer()
+                                Image("nextNarrativeButton")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(maxWidth: 50)
+                                    .padding(.trailing, 100)
+                                    .padding(.bottom, 300)
+                            }
                         }
                     }
                 }
             }
-            
         }
     }
 }
