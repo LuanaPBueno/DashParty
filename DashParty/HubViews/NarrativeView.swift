e2ae53a47f7455d785dbf310538fa566b6fb8d57
madumariano <madumarianofernandez@gmail.com>
2025-03-28 14:01:52 -0300

    frontEnd changes

--- DashParty/HubViews/NarrativeView.swift
+++ DashParty/HubViews/NarrativeView.swift
@@ -29,7 +29,7 @@ struct NarrativeView: View {
                     }
                 } else {
                     Text("Fim da narrativa!")
-                        .font(.custom("Prompt-Regular", size: fontSize))
+                        .font(.custom("Prompt-Regular", size: 28, relativeTo: .title))
                         .foregroundColor(.black)
                         .multilineTextAlignment(.center)
                 }
@@ -59,9 +59,9 @@ struct NarrativeView: View {
                     Text(hubManager.narrativeText[hubManager.actualPage].keys.first ?? "")
                         .lineLimit(nil)
                         .foregroundColor(.black)
-                        .font(.custom("Prompt-Regular", size: fontSize))
+                        .font(.custom("Prompt-Regular", size: 40, relativeTo: .title))
                         .multilineTextAlignment(.center)
-                        .padding(.horizontal, 80)
+                        .padding(.horizontal, 40)
                     
                         .frame(maxWidth: .infinity)
                     
@@ -90,7 +90,7 @@ struct NarrativeView: View {
                         }
                     }
                     ///MARK: TIRAR
-
+                    
                     
                 }
                 
@@ -101,134 +101,133 @@ struct NarrativeView: View {
     
     private func withCharacter() -> some View {
         
-        ZStack{
+        VStack{
             
-            Image("characters")
-                .resizable()
-                .scaledToFit()
-                
-            Spacer()
+
             
-            VStack {
-                Spacer()
-                VStack{
+            VStack{
                 
-                    Text(hubManager.narrativeText[hubManager.actualPage].keys.first ?? "")
-                        .foregroundColor(.black)
-                        .font(.custom("Prompt-Regular", size: fontSize))
-                        .padding(.horizontal, 80)
-                        .frame(maxWidth: .infinity)
-                    
-                        .background{
-                            Image("narrativeTextBackground")
-                                .resizable()
-                                .scaledToFill()
-                                .padding(.horizontal, 30) 
-                        }
-                  
-                }
                 Spacer()
-                        
-                }
-               
-                ZStack{
-                   
-                    if hubManager.actualPage == 4 {
-                        Image("monkeyIcon")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: iconSize, height: iconSize)
-                            .padding(.horizontal)
-                            
-                        
-                    }
-                    
-                    
-                    if hubManager.actualPage == 5 {
-                        Image("frogIcon")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: iconSize, height: iconSize)
-                            .padding(.horizontal)
-                        
-                    }
-                    
-                    if hubManager.actualPage == 6 {
-                        Image("bunnyIcon")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: iconSize, height: iconSize)
-                            .padding(.horizontal)
-                        
-                    }
-                    
-                    if hubManager.actualPage == 7 {
-                        Image("monkeyIcon")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: iconSize, height: iconSize)
-                            .padding(.horizontal)
-                        
-                    }
-                    
-                    if hubManager.actualPage == 8 {
-                        Image("catIcon")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: iconSize, height: iconSize)
-                            .padding(.horizontal)
-                        
-                    }
-                    
-                    if hubManager.actualPage == 9 {
-                        Image("monkeyIcon")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: iconSize, height: iconSize)
-                            .padding(.horizontal)
-                        
-                    }
-                    
-                    if hubManager.actualPage == 10 {
-                        Image("frogIcon")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: iconSize, height: iconSize)
-                            .padding(.horizontal)
-                        
-                    }
-                    Spacer()
-                }
-                    
-                    
-            ///MARK: TIRAR
-                    VStack{
-                        Button(action: {
-                            if hubManager.actualPage < hubManager.narrativeText.count - 1 {
-                                hubManager.actualPage += 1
-                            }
-                        }) {
-                            Text("Próximo")
-                                .font(.custom("Prompt-Regular", size: fontSize))
-                                .foregroundColor(.blue)
-                            
-                        }
-                    }
-            ///MARK: TIRAR
 
                 
-                Spacer()
-            }
-        
+                Text(hubManager.narrativeText[hubManager.actualPage].keys.first ?? "")
+                    .foregroundColor(.black)
+                    .font(.custom("Prompt-Regular", size: 40, relativeTo: .title))
+                    .padding(.horizontal, 80)
+                    .frame(maxWidth: .infinity)
+                
+                    .background{
+                        Image("narrativeTextBackground")
+                            .resizable()
+                            .scaledToFill()
+                            .padding(.horizontal, 30)
+                            .overlay (alignment: .top){
+                                ZStack{
+                                    
+                                    if hubManager.actualPage == 4 {
+                                        
+                                        Image("monkeyIcon")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: iconSize, height: iconSize)
+                                            .padding(.horizontal)
+                                        
+                                        
+                                    }
+                                    
+                                    
+                                    if hubManager.actualPage == 5 {
+                                        Image("frogIcon")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: iconSize, height: iconSize)
+                                            .padding(.horizontal)
+                                        
+                                    }
+                                    
+                                    if hubManager.actualPage == 6 {
+                                        Image("bunnyIcon")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: iconSize, height: iconSize)
+                                            .padding(.horizontal)
+                                        
+                                    }
+                                    
+                                    if hubManager.actualPage == 7 {
+                                        Image("monkeyIcon")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: iconSize, height: iconSize)
+                                            .padding(.horizontal)
+                                        
+                                    }
+                                    
+                                    if hubManager.actualPage == 8 {
+                                        Image("catIcon")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: iconSize, height: iconSize)
+                                            .padding(.horizontal)
+                                        
+                                    }
+                                    
+                                    if hubManager.actualPage == 9 {
+                                        Image("monkeyIcon")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: iconSize, height: iconSize)
+                                            .padding(.horizontal)
+                                        
+                                    }
+                                    
+                                    if hubManager.actualPage == 10 {
+                                        Image("frogIcon")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: iconSize, height: iconSize)
+                                            .padding(.horizontal)
+                                        
+                                    }
+                                    
+                                }
+                                .alignmentGuide(.top, computeValue: {dimension in dimension[.bottom] - 50})
+                            }
+                    }
+                    .background (alignment: .bottom){
+                        Image("characters")
+                            .resizable()
+                            .scaledToFill()
+                        
+                        
+                    }
+                    .alignmentGuide(.top, computeValue: {dimension in dimension[.top]})
+                
+                
+                
+            }.padding(.bottom, 16)
+            
+            
+            
+            
+            ///MARK: TIRAR
+                
+            
+            ///MARK: TIRAR
+            
+            
             
         }
+        
+        
     }
+}
 
-    
-    
-    #Preview{
-        NarrativeView( )
-    }
-    
-    
-    
+
+
+#Preview{
+    NarrativeView( )
+}
+
+
+
