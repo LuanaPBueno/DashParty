d1f2b0885a880a26a2e39d6b9a7c469ea87ebbe2
LuanaPueno <luanabuenorj@gmail.com>
2025-04-01 18:13:45 -0300

    mutipeer conectando porém com o nome dos jogadores não observável automaticamente pro host. Aceita no máximo 3 jogadores além do host

---
+++ DashParty/Configurations/MultipeerConnectivity/MutipeerControler.swift
@@ -0,0 +1,8 @@
+//
+//  MutipeerView.swift
+//  DashParty
+//
+//  Created by Luana Bueno on 31/03/25.
+//
+
+import Foundation
---
+++ GuessingView.swift
@@ -0,0 +1,18 @@
+//
+//  GuessingView.swift
+//  DashParty
+//
+//  Created by Luana Bueno on 31/03/25.
+//
+
+import SwiftUI
+
+struct GuessingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuessingView()
+}
