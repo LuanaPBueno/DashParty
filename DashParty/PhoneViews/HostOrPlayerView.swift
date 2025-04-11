d486d14f6a848889c0c72a8308f3f628df24e6c6
fernanda.auler <fernandaauler1@gmail.com>
2025-04-11 19:27:39 -0300

    alterando chave

--- DashParty/PhoneViews/HostOrPlayerView.swift
+++ DashParty/PhoneViews/HostOrPlayerView.swift
@@ -13,7 +13,7 @@ struct HostOrPlayerView: View {
     var multipeerSession : MPCSession = MPCSessionManager.shared
     
     @State var navigateToHost : Bool = false
-//    @State var navigateToJoin : Bool = false
+    //    @State var navigateToJoin : Bool = false
     @State var changed: Bool = HUBPhoneManager.instance.changeScreen
     @State private var isActive = false
     @State var showAlert = false
@@ -24,125 +24,128 @@ struct HostOrPlayerView: View {
     
     var body: some View {
         
-       
+        
+        
+        ZStack{
+            Image("darkblueBackground")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
             
-            ZStack{
-                Image("darkblueBackground")
-                    .resizable()
-                    .scaledToFill()
-                    .ignoresSafeArea()
+            VStack(alignment: .center, spacing: 10) {
                 
-                VStack(alignment: .center, spacing: 10) {
-                    
-                    Button(action: {
-                        MPCSessionManager.shared.host = true
-                        MPCSessionManager.shared.start()
-                      //   showRoomAlert = true //MARK: MUDAR ISSO
-                        if userName != ""{
-                            navigateToHost = true
-                        }else{
-                            askForHostName = true
-                        }
-                        MPCSessionManager.shared.startSession(asHost: true)
-
-                    }) {
-                        Image("decorativeRectOrange")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 150, height: 60)
-                            .overlay(
-                                Text("Host")
-                                    .font(.custom("TorukSC-Regular", size: 40))
-                                    .foregroundColor(.white)
-                            )
+                Button(action: {
+                    MPCSessionManager.shared.host = true
+                    MPCSessionManager.shared.start()
+                    //   showRoomAlert = true //MARK: MUDAR ISSO
+                    if userName != ""{
+                        navigateToHost = true
+                    }else{
+                        askForHostName = true
                     }
+                    MPCSessionManager.shared.startSession(asHost: true)
                     
-//                    NavigationLink(
-//                        destination: GameDifficulty(multipeerSession: multipeerSession),
-//                        isActive: $navigateToHost,
-//                        label: { EmptyView() }
-//                    )
-                    
-                    
-                    Button(action: {
-                        MPCSessionManager.shared.host = false
-                        MPCSessionManager.shared.startSession(asHost: false)
-                        if userName != ""{
-                            navigateToHost = true
-                        }else{
-                            showAlert = true
-                        }
-                    }) {
-                        Image("decorativeRectBlue")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 150, height: 60)
-                        
-                            .overlay(
-                                Text("Join")
-                                    .font(.custom("TorukSC-Regular", size: 40))
-                                    .foregroundColor(.white)
-                                
-                            )
-                    }
-                    
-                    
-                    
-                   
-                    
+                }) {
+                    Image("decorativeRectOrange")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 60)
+                        .overlay(
+                            Text("Host")
+                                .font(.custom("TorukSC-Regular", size: 40))
+                                .foregroundColor(.white)
+                        )
                 }
                 
-                .alert("Entre com seu nome", isPresented: $askForHostName) {
-                    TextField("Entre com seu nome", text: $userName)
-                        Button("Cancelar", role: .cancel) { }
-                        
-                            Button {
-                                HUBPhoneManager.instance.allPlayers[0].name = userName
-                                navigateToHost = true
-                            } label: {
-                                Text("Salvar")
-                            }
-
-                       }
-                
-                .alert("Entre com seu nome", isPresented: $showAlert) {
-                    TextField("Entre com seu nome", text: $userName)
-                        Button("Cancelar", role: .cancel) { }
-                        
-                            Button {
-                                HUBPhoneManager.instance.playername = userName
-                                showAlert = false
-                                router = .chooseRoom
-                            }
-
-                       }
-//                
-//                .alert("Entre com o nome da sala", isPresented: $showRoomAlert) {
-//                    TextField("Entre com o nome da sala", text: $userName)
-//                        Button("Cancelar", role: .cancel) { }
-//                        
-//                            Button {
-//                                HUBPhoneManager.instance.roomName = roomName
-//                                showAlert = false
-//                                navigateToHost = true
-//                                askForHostName = true
-//                                
-//                            } label: {
-//                                Text("Salvar")
-//                            }
-//
-//                       }
+                //                    NavigationLink(
+                //                        destination: GameDifficulty(multipeerSession: multipeerSession),
+                //                        isActive: $navigateToHost,
+                //                        label: { EmptyView() }
+                //                    )
                 
                 
-     
+                Button(action: {
+                    MPCSessionManager.shared.host = false
+                    MPCSessionManager.shared.startSession(asHost: false)
+                    if userName != ""{
+                        navigateToHost = true
+                    }else{
+                        showAlert = true
+                    }
+                }) {
+                    Image("decorativeRectBlue")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 60)
+                    
+                        .overlay(
+                            Text("Join")
+                                .font(.custom("TorukSC-Regular", size: 40))
+                                .foregroundColor(.white)
+                            
+                        )
+                }
                 
                 
-               
-
                 
-              
+                
+                
             }
+            
+            .alert("Entre com seu nome", isPresented: $askForHostName) {
+                TextField("Entre com seu nome", text: $userName)
+                Button("Cancelar", role: .cancel) { }
+                
+                Button {
+                    HUBPhoneManager.instance.allPlayers[0].name = userName
+                    navigateToHost = true
+                } label: {
+                    Text("Salvar")
+                }
+                
+            }
+            
+            .alert("Entre com seu nome", isPresented: $showAlert) {
+                TextField("Entre com seu nome", text: $userName)
+                Button("Cancelar", role: .cancel) { }
+                
+                Button {
+                    HUBPhoneManager.instance.playername = userName
+                    showAlert = false
+                    router = .chooseRoom
+                }
+                label: {
+                    Text("Salvar")
+                }
+                
+            }
+            //
+            //                .alert("Entre com o nome da sala", isPresented: $showRoomAlert) {
+            //                    TextField("Entre com o nome da sala", text: $userName)
+            //                        Button("Cancelar", role: .cancel) { }
+            //
+            //                            Button {
+            //                                HUBPhoneManager.instance.roomName = roomName
+            //                                showAlert = false
+            //                                navigateToHost = true
+            //                                askForHostName = true
+            //
+            //                            } label: {
+            //                                Text("Salvar")
+            //                            }
+            //
+            //                       }
+            
+            
+            
+            
+            
+            
+            
+            
+            
         }
+    }
     
 }
 
