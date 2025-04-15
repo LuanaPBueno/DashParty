7bab3ac5ba4c4696122f186e2ea85ea44e41c8ff
fernanda.auler <fernandaauler1@gmail.com>
2025-04-15 20:10:14 -0300

    narrative hub view bugs fixed after merge

--- DashParty/HubViews/TutorialHubView .swift
+++ DashParty/HubViews/TutorialHubView .swift
@@ -1,5 +1,5 @@
 //
-//  TutorialHubView .swift
+//  TutorialHubView.swift
 //  DashParty
 //
 //  Created by Luana Bueno on 26/03/25.
@@ -9,76 +9,45 @@ import Foundation
 import SwiftUI
 
 struct TutorialHubView: View {
-    @Binding var router:Router
+    @Binding var router: Router
     var multipeerSession = MPCSessionManager.shared
-    
+
     let user = HUBPhoneManager.instance.user
     @State var matchManager = HUBPhoneManager.instance.matchManager
-    var users: [User] = []
     var hubManager = HUBPhoneManager.instance
-    
-    var myPlayer: Player? {
-        matchManager.getPlayer(forUser: user.id)
-    }
-    
-    
-    var currentTutorialImage: [String] = ["tutorialhub1", "tutorialhub2", "tutorialhub3", "tutorialhub4", "tutorialhub5", "tutorialhub6" ]
-    
+
+    var currentTutorialImage: [String] = [
+        "tutorialhub1", "tutorialhub2", "tutorialhub3",
+        "tutorialhub4", "tutorialhub5", "tutorialhub6"
+    ]
+
     var body: some View {
-        if !hubManager.startMatch {
-            //if currentTutorialImage[safe: hubManager.actualTutorialIndex] == "tutorialhub6"{
-                
-            
-           // }else{
-            Image(currentTutorialImage[safe: hubManager.actualTutorialIndex] ?? "")
+        ZStack {
+            if !hubManager.startMatch {
+                // Exibe a imagem do tutorial atual
+                Image(currentTutorialImage[safe: hubManager.actualTutorialIndex] ?? "fallbackImage")
                     .resizable()
                     .scaledToFill()
-                    .edgesIgnoringSafeArea(.all)
-            //}
-        }else{
-            MatchGridView(router: $router, count: multipeerSession.mcSession.connectedPeers.count, user: user, matchManager: matchManager)
-                .task{
-                    print(multipeerSession.mcSession.connectedPeers.count)
+                    .ignoresSafeArea()
+            } else {
+                // Quando o match começa, muda para a tela MatchGrid
+                MatchGridView(
+                    router: $router,
+                    count: multipeerSession.mcSession.connectedPeers.count,
+                    user: user,
+                    matchManager: matchManager
+                )
+                .task {
+                    print("Connected Peers: \(multipeerSession.mcSession.connectedPeers.count)")
                 }
-            }else{
-                MatchGridView(router: $router, count: multipeerSession.mcSession.connectedPeers.count, user: user, matchManager: matchManager)
-                    .task{
-                        print(multipeerSession.mcSession.connectedPeers.count)
-                    }
             }
         }
-        
-        VStack{
-            if !hubManager.startMatch {
-                if currentTutorialImage[safe: hubManager.actualTutorialIndex] == "tutorialhub6"{
-                    
-                    //                Button {
-                    //                    router = .game
-                    //                } label: {
-                    //                    Image("startMatchButton")
-                    //                        .resizable()
-                    //                        .scaledToFit()
-                    //                        .frame(width: 100, height: 100)
-                    //                }
-                    
-                }else{
-                    
-                    Image(currentTutorialImage[safe: hubManager.actualTutorialIndex] ?? "")
-                        .resizable()
-                        .scaledToFill()
-                        .ignoresSafeArea()
-                        .edgesIgnoringSafeArea(.all)
-                }
-            }else{
-                MatchGridView(router: $router, count: multipeerSession.mcSession.connectedPeers.count, user: user, matchManager: matchManager)
-                    .task{
-                        print(multipeerSession.mcSession.connectedPeers.count)
-                    }
-            }
-            
+        .onAppear {
+            hubManager.actualTutorialIndex = 0 // começa sempre do início
         }
     }
 }
+
 extension Array {
     subscript(safe index: Int) -> Element? {
         return indices.contains(index) ? self[index] : nil
