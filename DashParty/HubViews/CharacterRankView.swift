bf832eb9bbf806e08552c8166da6466f30f4ff49
LuanaPueno <luanabuenorj@gmail.com>
2025-05-27 16:27:18 -0300

    Improve file organization.

--- DashParty/HubViews/CharacterRankView.swift
+++ DashParty/HubViews/CharacterRankView.swift
@@ -7,52 +7,6 @@
 
 import SwiftUI
 
-enum CharacterStatus {
-    case winner, regular, didnotfinish
-}
-
-enum CharacterColor: String {
-    case red, blue, yellow, green
-}
-
-enum KikoColor: String {
-    case red = "KikoRed"
-    case blue = "KikoBlue"
-    case yellow = "KikoYellow"
-    case green = "KikoGreen"
-}
-
-enum BannerType: String {
-    case winner = "bannerGold"
-    case regular = "bannerSilver"
-}
-
-struct CharacterFrameType {
-    var status: CharacterStatus
-    var color: CharacterColor
-    
-    var imageName: String {
-        switch status {
-        case .winner:
-            return "characterFrame\(color.rawValue.capitalized)Win"
-        case .regular:
-            return "characterFrame\(color.rawValue.capitalized)Player"
-        case .didnotfinish:
-            return "characterFrame\(color.rawValue.capitalized)DNF"
-        }
-    }
-}
-extension CharacterColor {
-    init(kikoColor: KikoColor) {
-        switch kikoColor {
-        case .red: self = .red
-        case .blue: self = .blue
-        case .yellow: self = .yellow
-        case .green: self = .green
-        }
-    }
-}
-
 struct CharacterRankView: View {
     var frameType: CharacterFrameType
     var kikoColor: KikoColor
---
+++ DashParty/Models/CharacterModels.swift
@@ -0,0 +1,53 @@
+//
+//  CharacterStatus.swift
+//  DashParty
+//
+//  Created by Luana Bueno on 27/05/25.
+//
+
+
+enum CharacterStatus {
+    case winner, regular, didnotfinish
+}
+
+enum CharacterColor: String {
+    case red, blue, yellow, green
+}
+
+enum KikoColor: String {
+    case red = "KikoRed"
+    case blue = "KikoBlue"
+    case yellow = "KikoYellow"
+    case green = "KikoGreen"
+}
+
+enum BannerType: String {
+    case winner = "bannerGold"
+    case regular = "bannerSilver"
+}
+
+struct CharacterFrameType {
+    var status: CharacterStatus
+    var color: CharacterColor
+    
+    var imageName: String {
+        switch status {
+        case .winner:
+            return "characterFrame\(color.rawValue.capitalized)Win"
+        case .regular:
+            return "characterFrame\(color.rawValue.capitalized)Player"
+        case .didnotfinish:
+            return "characterFrame\(color.rawValue.capitalized)DNF"
+        }
+    }
+}
+extension CharacterColor {
+    init(kikoColor: KikoColor) {
+        switch kikoColor {
+        case .red: self = .red
+        case .blue: self = .blue
+        case .yellow: self = .yellow
+        case .green: self = .green
+        }
+    }
+}
