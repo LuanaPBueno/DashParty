18cd4e9f3f19e631ac551b27daa4e792d97c475a
Visckmart <visckmart@hotmail.com>
2025-05-28 00:30:08 -0300

    Remove NearbyInteraction framework usage snippet.

--- DashParty/Configurations/AppDelegate.swift
+++ DashParty/Configurations/AppDelegate.swift
@@ -8,24 +8,9 @@
 
 import Foundation
 import SwiftUI
-import NearbyInteraction
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-        // Check Nearby Interaction device support
-        var isSupported: Bool
-        if #available(iOS 16.0, *) {
-            isSupported = NISession.deviceCapabilities.supportsPreciseDistanceMeasurement
-        } else {
-            isSupported = NISession.isSupported
-        }
-        
-        if !isSupported {
-            //print("Device doesn't support Nearby Interaction")
-            // You might want to show an alert or switch to a different view here
-            // For SwiftUI, you could set up a state variable to trigger an alert
-        }
-        
         return true
     }
     
